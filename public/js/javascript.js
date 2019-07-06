bbd45aad2ec5e57fea4fd23cfa30425edf35a136
Loydith Valdivia <loydith@gmail.com>
2019-07-06 15:10:21 -0500

    update project url

---
+++ public/js/javascript.js
@@ -0,0 +1,15 @@
+// function validateForm() {
+//     var x = document.forms["myForm"]["fname"].value;
+//     if (x == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     }
+//   }
+  // navBar with jQuery
+
+  $(document).ready(function(){
+    // $('.slider').slider({full_width: true});
+    $('.sidenav').sidenav();
+    
+    });
+  
